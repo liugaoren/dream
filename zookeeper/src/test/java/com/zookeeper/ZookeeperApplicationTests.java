237b8ad1fd8504917fe4ca91d27e5309de719685
liugaoren <gaoren.liu@nriat.com>
2021-12-31 10:36:44 +0800

    zookeeper初始测试

--- zookeeper/src/test/java/com/zookeeper/ZookeeperApplicationTests.java
+++ zookeeper/src/test/java/com/zookeeper/ZookeeperApplicationTests.java
@@ -1,23 +1,36 @@
 package com.zookeeper;
 
+import org.apache.curator.CuratorZookeeperClient;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.BackgroundCallback;
+import org.apache.curator.framework.api.CuratorEvent;
+import org.apache.curator.framework.imps.CuratorFrameworkImpl;
+import org.apache.curator.framework.imps.CuratorTempFrameworkImpl;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class ZookeeperApplicationTests {
+    CuratorFramework client =null;
 
-    @Test
+
+    @BeforeEach
     void contextLoads() {
         //重试策略
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(3000, 10);
         //2.第二种方式
         //CuratorFrameworkFactory.builder();
-        CuratorFramework client = CuratorFrameworkFactory.builder()
-                .connectString("172.22.254.135:2181")
+       client = CuratorFrameworkFactory.builder()
+                .connectString("localhost:2181")
                 .sessionTimeoutMs(60 * 1000)
                 .connectionTimeoutMs(15 * 1000)
                 .retryPolicy(retryPolicy)
@@ -27,4 +40,121 @@ class ZookeeperApplicationTests {
         client.start();
     }
 
+    @Test
+    public void testCreate() throws Exception {
+        //2. 创建节点 带有数据
+        //如果创建节点，没有指定数据，则默认将当前客户端的ip作为数据存储
+       String path = client.create().forPath("/app2", "hehe".getBytes());
+       //client.delete().forPath("/app3");
+        System.out.println(path);
+    }
+
+    @Test
+    public void testCreate2() throws Exception {
+        //1. 基本创建
+        //如果创建节点，没有指定数据，则默认将当前客户端的ip作为数据存储
+        String path = client.create().forPath("/app1");
+        System.out.println(path);
+    }
+    @Test
+    public void testCreate3() throws Exception {
+        //3. 设置节点的类型
+        //默认类型：持久化
+        String path = client.create().withMode(CreateMode.EPHEMERAL).forPath("/app3");
+        System.out.println(path);
+    }
+    @Test
+    public void testCreate4() throws Exception {
+        //4. 创建多级节点  /app4/p1
+        //creatingParentsIfNeeded():如果父节点不存在，则创建父节点
+        String path = client.create().creatingParentsIfNeeded().forPath("/app4/p1");
+        System.out.println(path);
+    }
+
+    /**
+     * 查询节点：
+     * 1. 查询数据：get: getData().forPath()
+     * 2. 查询子节点： ls: getChildren().forPath()
+     * 3. 查询节点状态信息：ls -s:getData().storingStatIn(状态对象).forPath()
+     */
+    @Test
+    public void testGet1() throws Exception {
+        //1. 查询数据：get
+        byte[] data = client.getData().forPath("/app1");
+        System.out.println(new String(data));
+    }
+
+    @Test
+    public void testGet2() throws Exception {
+        // 2. 查询子节点： ls
+        List<String> path = client.getChildren().forPath("/");
+        System.out.println(path);
+    }
+    @Test
+    public void testGet3() throws Exception {
+        Stat status = new Stat();
+        System.out.println(status);
+        //3. 查询节点状态信息：ls -s
+        client.getData().storingStatIn(status).forPath("/app1");
+        System.out.println(status);
+    }
+
+    /**
+     * 修改数据
+     * 1. 基本修改数据：setData().forPath()
+     * 2. 根据版本修改: setData().withVersion().forPath()
+     * * version 是通过查询出来的。目的就是为了让其他客户端或者线程不干扰我。
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testSet() throws Exception {
+        client.setData().forPath("/app1", "itcast".getBytes());
+    }
+
+    @Test
+    public void testSetForVersion() throws Exception {
+        Stat status = new Stat();
+        //3. 查询节点状态信息：ls -s
+        client.getData().storingStatIn(status).forPath("/app1");
+        int version = status.getVersion();//查询出来的 3
+        System.out.println(version);
+        client.setData().withVersion(version).forPath("/app1", "hehe".getBytes());
+    }
+
+    /**
+     * 删除节点： delete deleteall
+     * 1. 删除单个节点:delete().forPath("/app1");
+     * 2. 删除带有子节点的节点:delete().deletingChildrenIfNeeded().forPath("/app1");
+     * 3. 必须成功的删除:为了防止网络抖动。本质就是重试。  client.delete().guaranteed().forPath("/app2");
+     * 4. 回调：inBackground
+     * @throws Exception
+     */
+    @Test
+    public void testDelete() throws Exception {
+        // 1. 删除单个节点
+        client.delete().forPath("/app1");
+    }
+    @Test
+    public void testDelete2() throws Exception {
+        //2. 删除带有子节点的节点
+        client.delete().deletingChildrenIfNeeded().forPath("/app4");
+    }
+
+    @Test
+    public void testDelete3() throws Exception {
+        //3. 必须成功的删除
+        client.delete().guaranteed().forPath("/app2");
+    }
+    @Test
+    public void testDelete4() throws Exception {
+        //4. 回调
+        client.delete().guaranteed().inBackground(new BackgroundCallback(){
+            @Override
+            public void processResult(CuratorFramework client, CuratorEvent event) throws Exception {
+                System.out.println("我被删除了~");
+                System.out.println(event);
+            }
+        }).forPath("/app1");
+    }
 }
