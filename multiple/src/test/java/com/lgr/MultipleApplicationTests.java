2e3ac50738b36c9162d2b85919df50a66ec6cf55
liugaoren <734055580@qq.com>
2022-05-08 20:19:21 +0800

    选择排序

--- multiple/src/test/java/com/lgr/MultipleApplicationTests.java
+++ multiple/src/test/java/com/lgr/MultipleApplicationTests.java
@@ -9,5 +9,19 @@ class MultipleApplicationTests {
     @Test
     void contextLoads() {
     }
-
+    //选择排序
+    public void selectSort(int[] arr){
+        int temp;
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+    }
 }
